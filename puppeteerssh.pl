e78fb1ad0e40d062ba07f51391f905c134c0b3af
Alexandros Kechagias <osakech@gmail.com>
2017-02-03 11:29:05 +0100

    perltidy

--- puppeteerssh.pl
+++ puppeteerssh.pl
@@ -5,8 +5,8 @@
 #
 #        USAGE: ./puppeteerssh.pl
 #
-#  DESCRIPTION: Lets you copy a program of your choice on multiple servers and 
-#               then execute it and then collect the output data from the server 
+#  DESCRIPTION: Lets you copy a program of your choice on multiple servers and
+#               then execute it and then collect the output data from the server
 #               and merge the output on your local machine.
 #
 #       AUTHOR: Alexandros Kechagias (osakech@gmail.com),
@@ -34,25 +34,22 @@ use PuppeteerSSH::Groupfiles 0.1;
 my $cliParams = PuppeteerSSH::Cli::getCliParams();
 
 # move to Groupfiles
-my $inputData = PuppeteerSSH::Groupfiles->new( $cliParams->{gfile}, $cliParams->{gdsh});
+my $inputData = PuppeteerSSH::Groupfiles->new( $cliParams->{gfile}, $cliParams->{gdsh} );
 my $serverArray = $inputData->getServers;
-
 
 my $numberOfServers = scalar @$serverArray;
 my $numberOfConnections = $cliParams->{'num_connections'} ? $cliParams->{'num_connections'} : $numberOfServers;
 say "Number of servers from the configfiles : $numberOfServers";
 say "Number of maximum parallel connections : $numberOfConnections";
-say "Connecting to the following servers :\n" . join "\n", map { " * ".$_ } @$serverArray;
+say "Connecting to the following servers :\n" . join "\n", map { " * " . $_ } @$serverArray;
 
-my $pm = Parallel::ForkManager->new( $numberOfConnections );
+my $pm = Parallel::ForkManager->new($numberOfConnections);
 
 # data structure retrieval and handling
 my @tmpFilePaths;
 $pm->run_on_finish(    # called BEFORE the first call to start()
     sub {
-        my ( $pid, $exit_code, $ident, $exit_signal, $core_dump,
-            $data_structure_reference )
-          = @_;
+        my ( $pid, $exit_code, $ident, $exit_signal, $core_dump, $data_structure_reference ) = @_;
         push @tmpFilePaths, $$data_structure_reference
           if ($data_structure_reference);
     }
@@ -63,8 +60,8 @@ foreach my $server (@$serverArray) {
     $pm->start() and next DATA_LOOP;
     chomp $server;
     my $sshConnection = PuppeteerSSH::SSH->new( $server, $cliParams->{ssh_option} ) or croak "Couldn't connect to server $server";
-    $sshConnection->putFileOnServer( $cliParams->{destination}, $cliParams->{script});
-    $sshConnection->executeOnServer( $cliParams->{destination}, $cliParams->{script});
+    $sshConnection->putFileOnServer( $cliParams->{destination}, $cliParams->{script} );
+    $sshConnection->executeOnServer( $cliParams->{destination}, $cliParams->{script} );
     my $copiedTo = $sshConnection->getFileFromServer( $cliParams->{resultfile} );
     $pm->finish( 0, \$copiedTo );
 }
