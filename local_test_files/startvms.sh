cc9e546d45ca11ed370a9db77ef0ea76541c15f0
Alexandros Kechagias <osakech@gmail.com>
2017-06-04 16:13:38 +0200

    add travis-ci support

--- local_test_files/startvms.sh
+++ local_test_files/startvms.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker start 'ssh1' 'ssh2' 'ssh3' 'ssh4';
-docker ps | perl -ne 's/0.0.0.0/localhost/g;/\s(localhost:\d+)->/;print "root@".$1."\n" if $1 ' > ~/github/netscpwrapper/local_test_files/mygroupfile;
-chown $USER:$USER ~/github/netscpwrapper/local_test_files/mygroupfile;
-chmod 777 ~/github/netscpwrapper/local_test_files/mygroupfile;
-cat ~/github/netscpwrapper/local_test_files/mygroupfile;
+docker ps | perl -ne 's/0.0.0.0/localhost/g;/\s(localhost:\d+)->/;print "root:screencast@".$1."\n" if $1 ' > local_test_files/mygroupfile;
+chown $USER:$USER local_test_files/mygroupfile;
+chmod 777 local_test_files/mygroupfile;
